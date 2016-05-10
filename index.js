ca989727e0ba7bc5b4206c42e19c41b9b3030e45
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2016-05-10 02:17:54 +0200

    feat(): add plugin auto-loading

--- index.js
+++ index.js
@@ -4,24 +4,27 @@
 
 'use strict'
 
-let loaderUtils = require('loader-utils')
+var loaderUtils = require('loader-utils')
 
-let posthtml = require('posthtml')
+var posthtml = require('posthtml')
+var postload = require('posthtml-load-plugins')
 
 module.exports = function (source) {
   if (this.cacheable) this.cacheable()
 
-  var plugins = this.options.posthtml || []
+  var plugins = this.options.posthtml || postload() || []
   var options = loaderUtils.parseQuery(this.query)
 
   if (typeof plugins === 'function') {
     plugins = plugins.call(this, this)
   }
+
   if (options.pack) {
     plugins = plugins[options.pack]
   }
+
   if (typeof plugins === 'undefined' || !options.pack && typeof plugins === 'object') {
-    plugins = plugins.defaults || []
+    plugins = plugins.defaults || postload() || []
   }
 
   var loader = this
@@ -31,7 +34,7 @@ module.exports = function (source) {
     .process(source.toString())
     .then(result => callback(null, result.html))
     .catch((error) => {
-      if (error.name === 'HTML Syntax Error') {
+      if (error.name === 'Error: PostHTML Loader') {
         loader.emitError(error.message + error.showSourceCode())
         callback()
       } else {
